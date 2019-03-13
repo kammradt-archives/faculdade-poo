6cb9c12e870c501f42fe0e2d9d22a7e3005972ad
kammradt <vinicius.kammradt1@gmail.com>
2019-03-12 22:16:05 -0300

    NEW: Classes from exercise three finished.

---
+++ src/lista3/Operacoes/Divisao.java
@@ -0,0 +1,9 @@
+package lista3.Operacoes;
+
+public class Divisao extends OperacaoMatematica {
+
+    @Override
+    public double calcula(double n1, double n2) {
+        return n1/n2;
+    }
+}
---
+++ src/lista3/Operacoes/OperacaoMatematica.java
@@ -0,0 +1,5 @@
+package lista3.Operacoes;
+
+public abstract class OperacaoMatematica {
+    abstract public double calcula(double n1, double n2);
+}
